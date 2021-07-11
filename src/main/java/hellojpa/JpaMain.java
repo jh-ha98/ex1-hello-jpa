9fbb7c43797338d8b2d6977c8b99b4f0a8e99d93
JH <jeongha182@gmail.com>
2021-07-12 00:19:04 +0900

    initial commit

---
+++ src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,34 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+
+	public static void main(String[] args) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("h2");
+		
+		EntityManager em = emf.createEntityManager();
+		EntityTransaction tx = em.getTransaction();
+
+		try {
+			tx.begin();
+			
+			Member member = em.find(Member.class, 1L);
+			
+			em.remove(member);
+			
+			tx.commit();
+		} 
+		catch (Exception e) {
+			tx.rollback();
+		} 
+		finally {
+			em.close();	
+		}
+		
+		emf.close();
+	}
+}
