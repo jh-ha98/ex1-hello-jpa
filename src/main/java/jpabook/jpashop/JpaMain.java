ded3b1494c1870acaa1e5281ccf0dbfa40f14b30
JH <jeongha182@gmail.com>
2021-08-15 15:52:22 +0900

    New project

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,25 +1,33 @@
-package hellojpa;
+package jpabook.jpashop;
+
+import java.lang.reflect.Member;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
+import jpabook.jpashop.domain.Order;
+
+
 public class JpaMain {
 
 	public static void main(String[] args) {
-		// 정하는 예뻐요~s2!
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("h2");
 		
 		EntityManager em = emf.createEntityManager();
+		
 		EntityTransaction tx = em.getTransaction();
-
+		tx.begin();
+		
 		try {
-			tx.begin();
 			
-			Member member = em.find(Member.class, 1L);
+			Order order = em.find(Order.class, 1L);
+			Long memberId = order.getMemberId();
 			
-			em.remove(member);
+			Member member = em.find(Member.class, memberId);
+			
+			
 			
 			tx.commit();
 		} 
@@ -32,4 +40,5 @@ public class JpaMain {
 		
 		emf.close();
 	}
+
 }
---
+++ src/main/java/jpabook/jpashop/domain/OrderStatus.java
@@ -0,0 +1,6 @@
+package jpabook.jpashop.domain;
+
+public enum OrderStatus {		//열거 타입 - 상수들을 모아 정의 
+
+	ORDER, CANCEL
+}
